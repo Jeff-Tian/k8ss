e3fd8063d70aff3051d47e7b75e4ac87884e766a
Jeff Tian <jie.tian@unicareer.com>
2019-05-28 16:02:32 +0800

    the first workable version

--- src/__tests__/cli.test.ts
+++ src/__tests__/cli.test.ts
@@ -3,15 +3,15 @@ import path from 'path';
 
 test('cli starts', async () => {
   await coffee
-    .fork('src/index.ts', ['cluster', 'namespace'], {
+    .fork('src/index.ts', ['switch', '--cluster=hello', '--namespace=world'], {
       env: {
         TS_NODE_PROJECT: path.resolve(__dirname, '../../tsconfig.json'),
       },
       execArgv: ['--require', require.resolve('ts-node/register/type-check')],
       cwd: path.resolve(__dirname, '../../'),
     })
-    .expect('stdout', /^switching.../)
-    .expect('stderr', '')
-    .expect('code', 0)
+    .expect('stdout', /^switching to  hello world .../)
+    .expect('stderr', '没有找到目标文件： /Users/jefftian/k8s-config/hello/config\n')
+    .expect('code', 1)
     .end();
 });
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import { Argv } from 'yargs';
-
-console.info('switching...');
+import fs from 'fs';
+import * as path from 'path';
+import * as os from 'os';
+import rimraf from 'rimraf';
+import shelljs from 'shelljs';
 
 require('yargs').command(
   'switch',
@@ -17,6 +20,48 @@ require('yargs').command(
       });
   },
   (args: any) => {
-    console.log(args.cluster, args.namepsace);
+    console.log('switching to ', args.cluster, args.namespace, '...');
+    const { cluster, namespace } = args;
+
+    const source = path.resolve(os.homedir(), 'k8s-config');
+    const sourceConfig = path.resolve(source, cluster, 'config');
+    console.log('will copy ', sourceConfig);
+
+    if (!fs.existsSync(sourceConfig)) {
+      console.error('没有找到目标文件：', sourceConfig);
+      process.exit(1);
+    }
+
+    const kubeFolder = path.resolve(os.homedir(), '.kube');
+    const kubeBackupFolder = path.resolve(os.homedir(), '.kube.bak');
+
+    if (fs.existsSync(kubeFolder)) {
+      if (fs.existsSync(kubeBackupFolder)) {
+        rimraf.sync(kubeBackupFolder);
+        console.log('deleted ', kubeBackupFolder);
+      }
+
+      fs.renameSync(kubeFolder, kubeBackupFolder);
+      console.log('backed up ~/.kube to ~/.kube.bak');
+    }
+
+    fs.mkdirSync(kubeFolder, { recursive: true });
+    console.log('created ', kubeFolder);
+
+    const targetConfig = path.resolve(kubeFolder, 'config');
+    fs.copyFileSync(sourceConfig, targetConfig);
+    console.log('copied from ', sourceConfig, ' to ', targetConfig);
+
+    console.log('will switching context...', namespace);
+    const output = shelljs.exec(
+      'kubectl config set-context $(kubectl config current-context)' + ' --namespace=' + namespace,
+    );
+    console.log('switched result: ');
+    console.log(output.stdout);
+
+    if (output.stderr) {
+      console.error(output.stderr);
+      process.exit(1);
+    }
   },
-);
+).argv;
