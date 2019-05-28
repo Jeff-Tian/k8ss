ed395f5d1629d03b8325a7b1a97c9f1bcfadaf26
Jeff Tian <jie.tian@unicareer.com>
2019-05-28 14:54:41 +0800

    assert switching...

--- src/__tests__/cli.test.ts
+++ src/__tests__/cli.test.ts
@@ -2,11 +2,16 @@ import coffee from 'coffee';
 import path from 'path';
 
 test('cli starts', async () => {
-  await coffee.fork('../index.ts', ['cluster', 'namespace'], {
-    env: {
-      TS_NODE_PROJECT: path.resolve(__dirname, './fixtures/tsconfig.json'),
-    },
-    execArgv: ['--require', require.resolve('ts-node/register/type-check')],
-    cwd: path.resolve(__dirname, '../'),
-  });
+  await coffee
+    .fork('src/index.ts', ['cluster', 'namespace'], {
+      env: {
+        TS_NODE_PROJECT: path.resolve(__dirname, '../../tsconfig.json'),
+      },
+      execArgv: ['--require', require.resolve('ts-node/register/type-check')],
+      cwd: path.resolve(__dirname, '../../'),
+    })
+    .expect('stdout', /^switching.../)
+    .expect('stderr', '')
+    .expect('code', 0)
+    .end();
 });
