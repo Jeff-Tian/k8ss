d825ed22181bb3bfeb6ff160f5434e1177a0ce45
Jeff Tian <jeff.tian@outlook.com>
2019-06-23 18:26:17 +0800

    :bug: fix space

--- src/index.ts
+++ src/index.ts
@@ -59,7 +59,7 @@ require('yargs').command(
     const currentContext = shelljs.exec('kubectl config current-context');
 
     console.log('current context = ', currentContext);
-    const output = shelljs.exec('kubectl config set-context' + currentContext + ' --namespace=' + namespace);
+    const output = shelljs.exec('kubectl config set-context ' + currentContext.stdout.replace('\n', '') + ' --namespace=' + namespace);
     console.log('switched result: ');
     console.log(output.stdout);
 
