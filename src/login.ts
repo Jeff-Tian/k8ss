8f191e78e10f0975bfe17f4780304f0afddb4ff9
Jeff Tian@Workstation <jie.tian@live.cn>
2022-03-26 18:59:22 +0800

    feat: getting device_code and user_code from keycloak

--- src/login.ts
+++ src/login.ts
@@ -1,3 +1,28 @@
-export const login = () => {
-  console.log('logged in');
+import axios from 'axios';
+import qs from 'qs';
+
+const getUserCodeAndDeviceCode = async () => {
+  try {
+    const { data } = await axios({
+      method: 'post',
+      url: 'https://keycloak.jiwai.win/auth/realms/UniHeart/protocol/openid-connect/auth/device',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      data: qs.stringify({
+        client_id: 'demoapp',
+      }),
+    });
+
+    return data;
+  } catch (ex) {
+    console.error(ex);
+    throw ex;
+  }
+};
+
+export const login = async () => {
+  console.log('getting user code and device code...');
+  const codes = await getUserCodeAndDeviceCode();
+  console.log('codes = ', codes);
 };
