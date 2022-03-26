aa737e52521d950a77901523888dfc526a075303
Jeff Tian@Workstation <jie.tian@live.cn>
2022-03-26 19:32:26 +0800

    feat: guide user to verification_uri and input user_code

--- src/login.ts
+++ src/login.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import qs from 'qs';
+import { exec } from 'child_process';
 
 const getUserCodeAndDeviceCode = async () => {
   try {
@@ -21,8 +22,15 @@ const getUserCodeAndDeviceCode = async () => {
   }
 };
 
+const openBrowser = async ({ verification_uri, user_code }: Record<string, string>) => {
+  console.log('请打开浏览器并浏览至： ', verification_uri, ' , 然后在打开的页面中输入 ', user_code);
+
+  exec(`start ${verification_uri}`);
+};
+
 export const login = async () => {
   console.log('getting user code and device code...');
   const codes = await getUserCodeAndDeviceCode();
   console.log('codes = ', codes);
+  await openBrowser(codes);
 };
